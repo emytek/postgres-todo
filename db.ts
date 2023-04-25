f372e19ab96cc683eb0cfe4c9a5f223ca6b4a70c
emytek <66952283+emytek@users.noreply.github.com>
2023-04-25 09:04:00 +0100

    env

--- db.ts
+++ db.ts
@@ -1,19 +1,20 @@
 const Pool = require('pg').Pool
 require('dotenv').config()
 
-// const pool = new Pool({
-//     user: process.env.USERNAME,
-//     password: process.env.PASSWORD,
-//     host: process.env.HOST,
-//     port: process.env.DBPORT,
-//     database: 'todo',
-// })
 const pool = new Pool({
-    user: 'postgres',
-    password: 'jacksontech',
-    host: 'localhost',
-    port: 5432,
+    user: process.env.DUSER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
     database: 'todo',
 })
+console.log(process.env.DBPORT, "*****")
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'jacksontech',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'todo',
+// })
 
 export default pool;
